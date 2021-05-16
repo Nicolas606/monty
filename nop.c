b9e8b60b3cef65a1f63ff1f81fdc28a5f83b31fb
Nicolas <castillonicolas606@gmail.com>
2021-05-16 04:33:39 +0000

    The file  that contains the  function doesn´t do anything

---
+++ nop.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+/**
+ * nop - function doesn´t do anything
+ *
+ * @head: number the stack
+ * @line: number the line that execute program
+ *
+ */
+
+void nop(stack_t **head, unsigned int line)
+{
+	if (head && line)
+	{
+		;
+	}
+}

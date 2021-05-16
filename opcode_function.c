e8243b2980088142987eac320d96196c890a5cf0
Nicolas <castillonicolas606@gmail.com>
2021-05-16 04:24:55 +0000

    File contain the funtion the open file and find the opcode

---
+++ opcode_function.c
@@ -0,0 +1,42 @@
+#include "monty.h"
+
+/**
+ * opcode_function - funtion the open file
+ *
+ * @opcode: name of the opcode
+ * @n: number the stack
+ * @line_n: number the line the function
+ *
+ */
+
+void opcode_function(char *opcode, char *n, unsigned int line_n)
+{
+	int i, find = 0;
+
+	instruction_t array[] = {
+			{"push", push},
+			{"pall", pall},
+			{"pint", pint},
+			{"pop", pop},
+			{"swap", swap},
+			{"add", add},
+			{"nop", nop},
+			{NULL, NULL}};
+	for (i = 0; array[i].opcode; i++)
+	{
+		if (strcmp(opcode, array[i].opcode) == 0)
+		{
+			find = i;
+			break;
+		}
+	}
+	if (find != i)
+	{
+		fprintf(stderr, "L%d: unknown instruction %s\n", line_n, opcode);
+		exit(EXIT_FAILURE);
+	}
+	if (find == 0)
+		new_node(n, line_n);
+	else
+		array[find].f(&head, line_n);
+}

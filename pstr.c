09788d99eafb3178fa3344d6d3c462fd17d2ebb6
Nicolas <castillonicolas606@gmail.com>
2021-05-17 04:04:48 +0000

    If the stack is empty, print only a new line

--- pstr.c
+++ pstr.c
@@ -14,7 +14,10 @@ void pstr(stack_t **head, unsigned int line)
 	stack_t *copy = *head;
 	(void)line;
 	if (head == NULL || (*head) == NULL)
+	{
+		printf("\n");
 		return;
+	}
 	while ((*head)->next != NULL)
 	{
 		if ((*head)->n < 0 || (*head)->n > 127)

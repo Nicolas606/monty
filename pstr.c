f041ea540ee90ee838e12834deedd5b773714043
Nicolas <castillonicolas606@gmail.com>
2021-05-17 04:02:12 +0000

    Fix if have only one node

--- pstr.c
+++ pstr.c
@@ -15,7 +15,6 @@ void pstr(stack_t **head, unsigned int line)
 	(void)line;
 	if (head == NULL || (*head) == NULL)
 		return;
-
 	while ((*head)->next != NULL)
 	{
 		if ((*head)->n < 0 || (*head)->n > 127)
@@ -33,4 +32,5 @@ void pstr(stack_t **head, unsigned int line)
 		copy = copy->next;
 		*head = copy;
 	}
+	printf("%c\n", (*head)->n);
 }
